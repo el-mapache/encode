0f35cbae4492ac31db4c446c14e84d51a4b64b5c
Adam Biagianti <adam.biagianti@gmail.com>
2012-04-01 11:47:04 -0700

    cleans up some formatting in requestHandler.js

--- requestHandler.js
+++ requestHandler.js
@@ -10,238 +10,224 @@ var childOpts = {cwd: 'data/uploads'};
 var upload = function(res, request){
     console.log("Request Handler 'upload' was called.");
     var form = new formidable.IncomingForm();
-    form.uploadDir = './data/incoming/';
+    		form.uploadDir = './data/incoming/';
     console.log('About to parse');
     console.log(form);
+
     fs.readdir('./data/uploads', function(err, files){
-        if(err) console.log(err);
-        //add ability to hook into this is display all user's uploaded files
-        if(files) {
-          console.log("here are some file" + files);
-        }
+    	if(err) console.log(err);
+      //add ability to hook into this is display all user's uploaded files
+      if(files) {
+      	console.log("here are some file" + files);
+      }
     });
     
     form.parse(request, function(error, fields, files) {
-        if(error) {
-            console.log(error);
-            res.writeHead(500, {'content-type' : 'text/plain'});
-            res.write('There was an error processing you request. Please try again.');
-            res.end;
-        }
+    	if(error) {
+      	console.log(error);
+        res.writeHead(500, {'content-type' : 'text/plain'});
+        res.write('There was an error processing you request. Please try again.');
+        res.end;
+      }
         
-        console.log(files);
-        console.log(fields);
-        var codecParse = fields.format.split('.'),
-            codec = codecParse[0],
-            format = codecParse[1],
-            tracks = fields.numTracks,
-            sampleRate = fields.sampleRate,
-            rate = fields.bitRate,
-            formatExt = '.' + codecParse[1],
-            file = files.fileToUpload.name,
-            clientEmail,
-            browserDL,
-            date = new Date(),
-            output = file.split('.'),
-            finalOutput = output[0]+'_'+date+alphaRandom()+formatExt;
-        
+	     console.log(files);
+	     console.log(fields);
+	     var codecParse  = fields.format.split('.'),
+		       codec       = codecParse[0],
+		       format      = codecParse[1],
+		       tracks      = fields.numTracks,
+		       sampleRate  = fields.sampleRate,
+		       rate        = fields.bitRate,
+		       formatExt   = '.' + codecParse[1],
+		       file        = files.fileToUpload.name,
+		       clientEmail = '',
+		       browserDL   = '',
+		       date        = new Date(),
+		       output      = file.split('.'),
+		       finalOutput = output[0]+'_'+date+alphaRandom()+formatExt;
+    
         if(fields.email != '') {
-            clientEmail = fields.email;
+       		clientEmail = fields.email;
         } else{ 
-            clientEmail = false;
+         	clientEmail = false;
         }
         
         if(fields.noEmail == 'on') {
           browserDL = true;
         } else{ 
-            browserDL = false;
+          browserDL = false;
         }
         
         console.log('parsing done ' + date);
        
         var ffmpeg = spawn('ffmpeg',
                            ['-y','-i', file, '-acodec', codec, '-ar', sampleRate, '-ac', tracks, '-ab',
-                            rate, '-f', format, finalOutput],childOpts
-                     );
+                            rate, '-f', format, finalOutput],childOpts);
      
         fs.rename(files.fileToUpload.path, "./data/uploads/" + file, function(err) {
-            console.log('renaming file');
-            if(err) console.log(err);
-            convert(ffmpeg, res, finalOutput, clientEmail, browserDL);
-        });
+        	console.log('renaming file');
+          if(err) console.log(err);
+         	convert(ffmpeg, res, finalOutput, clientEmail, browserDL);
+       	});
     });    
 }
 
 var convert = function(ffmpeg, res, finalOutput, clientEmail, browserDL) {
-            console.log('convert called');
-            ffmpeg.stderr.setEncoding('utf8');
-            var temp,
-                lines,
-                time = [],
-                finalTime,
-                timeUp;
-            res.writeHead(200, {'Content-type' : 'type/html'});
+	console.log('convert called');
+	ffmpeg.stderr.setEncoding('utf8');
+	var temp,
+	    lines,
+	    time = [],
+	    finalTime,
+	    timeUp;
+	res.writeHead(200, {'Content-type' : 'type/html'});
 
-            ffmpeg.stdout.on('data', function(data) {
-                
-                //console.log(data);
-            });
-            
-            ffmpeg.stderr.on('data', function (data) {
-                temp = data.toString();
-                lines = temp.split('\n');
-                for(var i = 0;i<lines.length;i++) {
-                  if(lines[i].match(/Duration/)) {
-                    var duration = lines[i];
-                    var finalTime =  duration.match(/\d[0-9]+\:\d[0-9]\:\d[0-9]\.\d[0-9]/);
-                    //finalTime = rawFinalTime.split(':').join()
-                    //console.log('here is duration ' + finalTime);
-                  }
-                 //console.log('this is array: ' + i + '  ' + lines[i]);
-                    if(lines[i].match(/time=\d[0-9]+\:\d[0-9]\:\d[0-9]\.\d[0-9]/)) {          
-                      time = lines[i].split('=');   
-                    }
-                    
-                    if(!(time.length < 1)) {
-                      //console.log(time[2]);
-                      timeUp = time[2].match(/\d[0-9]+\:\d[0-9]\:\d[0-9]\.\d[0-9]/);
-                      console.log(timeUp);
-                      //res.write(timeUp[0] + ' ');
-                      //var bytesUploaded = timeUp[0];
-                      //var bytesTotal = finalTime;
-                      //var pc = parseInt(100 - (timeUp[0] / finalTime * 100));
-                   // res.write(timeUp[0]+'<br />');
-//res.write(pc);
-                      //console.log(timeUp[0]);
-                     
-                    }
-                }
-            });
+	ffmpeg.stdout.on('data', function(data) {
+    
+	});
 
-            ffmpeg.on('exit', function(code) {
-                res.write('<br /><span>Your file was uploaded successfully!</span>');
-                if(browserDL) {
-                  res.write("<br /><a id='download-button' href='/download?url="+finalOutput+"'>click to download your file</a>"); 
-                }
-                res.end();
-                console.log('child process ffmpeg exited with code ' + code);
-                onSuccess(code, finalOutput, clientEmail);
-            });
+	ffmpeg.stderr.on('data', function (data) {
+	  temp = data.toString();
+	  lines = temp.split('\n');
+	  for(var i = 0;i<lines.length;i++) {
+	    if(lines[i].match(/Duration/)) {
+	      var duration = lines[i];
+	      var finalTime =  duration.match(/\d[0-9]+\:\d[0-9]\:\d[0-9]\.\d[0-9]/);
+	    }
+	      if(lines[i].match(/time=\d[0-9]+\:\d[0-9]\:\d[0-9]\.\d[0-9]/)) {          
+	        time = lines[i].split('=');   
+	      }
+     
+	      if(!(time.length < 1)) {
+	        timeUp = time[2].match(/\d[0-9]+\:\d[0-9]\:\d[0-9]\.\d[0-9]/);
+	      }
+	  }
+	});
+
+	ffmpeg.on('exit', function(code) {
+	  res.write('<br /><span>Your file was uploaded successfully!</span>');
+	  if(browserDL) {
+	    res.write("<br /><a id='download-button' href='/download?url="+finalOutput+"'>click to download your file</a>"); 
+	  }
+	  res.end();
+	  console.log('child process ffmpeg exited with code ' + code);
+	  onSuccess(code, finalOutput, clientEmail);
+	});
 }
 
 var onSuccess = function(code, finalOutput, clientEmail) {
-    var redis = require("redis");
-    var client = redis.createClient();
-    console.log('success test called');
-    console.log(clientEmail);
-    var downloadKey;
-    if(code == 0) {
-      do {
-        downloadKey = alphaRandom();
-      } while (client.exists(downloadKey, function(err) {console.log('while loop error?:   '+err);}));
-      
-        client.on("error", function (err) {
-            console.log("Error " + err);
-        });
-        
-        client.on('connect', function() {
-          console.log('redis connection established, saving key: ' + downloadKey);
-          console.log('value for redis store:  '+finalOutput);
-          client.set(downloadKey, finalOutput, redis.print);
-          client.expire(downloadKey, 
-                        settings.settings.redisCacheTime, 
-                        redis.print);
-          client.get(downloadKey, redis.print);          
-        });
-         
-        if(clientEmail) {
-          var linkToURL = "<a href='" + settings.settings.downloadFrom + "/show?url=" 
-                            + downloadKey + "'>Click to visit your download page.</a>";
-          var plain = settings.settings.downloadFrom + "/show?url="+downloadKey;
-          email.emailer(clientEmail,linkToURL,plain);
-        }
+	var redis = require("redis");
+	var client = redis.createClient();
+	console.log('success test called');
+	console.log(clientEmail);
+	var downloadKey;
+	if(code == 0) {
+	  do {
+	    downloadKey = alphaRandom();
+	  } while (client.exists(downloadKey, function(err) {console.log('while loop error?:   '+err);}));
   
-    } else {
-        console.log('no dice on that file upload conejito');
-    }
+	    client.on("error", function (err) {
+	        console.log("Error " + err);
+	    });
     
-    client.on('end', function(err) {
-      console.log('something went wrong with redis');
-      client.quit();
-    });
+	    client.on('connect', function() {
+	    console.log('redis connection established, saving key: ' + downloadKey);
+	    console.log('value for redis store:  '+finalOutput);
+	    client.set(downloadKey, finalOutput, redis.print);
+	    client.expire(downloadKey, settings.settings.redisCacheTime, redis.print);
+	      client.get(downloadKey, redis.print);          
+	    });
+     
+	    if(clientEmail) {
+	      var linkToURL = "<a href='" + settings.settings.downloadFrom + "/show?url=" 
+	                        + downloadKey + "'>Click to visit your download page.</a>";
+	      var plain = settings.settings.downloadFrom + "/show?url="+downloadKey;
+	      email.emailer(clientEmail,linkToURL,plain);
+	    }
+
+	} else {
+	    console.log('no dice on that file upload conejito');
+	}
+
+	client.on('end', function(err) {
+	  console.log('something went wrong with redis');
+	  client.quit();
+	});
 }
 
 var show = function(res, req, query) {
   
-  var redis = require("redis");
-  var client = redis.createClient();
-  console.log('show function called with query string: ' +query);
-  console.log(client);
-  
-  client.on("error", function (err) {
-    console.log("Error " + err);
-  });
+	var redis = require("redis");
+	var client = redis.createClient();
+	console.log('show function called with query string: ' +query);
+	console.log(client);
 
-  client.get(query, function(err, reply) {
-    if(err) {
-        console.log('Error querying the database, returned:  ' + err);
-    }
-        console.log(reply);
-        fs.readdir(__dirname + '/data/uploads/', function(err, files) {
-          console.log(files);
-            if(err) {
-              console.log(err);
-              res.writeHead(500, {'content-type' : 'text/plain'});
-                res.write('There was an error processing your request. Please try again.');
-                res.end;
-            }
-  
-            for(var i = 0;i<files.length;i++) {
-              if(files[i] === reply) {
-                console.log(files[i]);
-                res.writeHeader(200, {'Content-Type':'text.html'});
-                res.write('<a href=/download?url='+encodeURI(files[i])+'>Click to download your converted file</a>');
-                res.end();
-              }
-            }
-              
-            res.writeHead(404, {'Content-Type' : 'text/html'});
-            res.end('No files found.  Your download link has likely expired');   
-        });
-    client.quit();
-  });
+	client.on("error", function (err) {
+	  console.log("Error " + err);
+	});
+
+	client.get(query, function(err, reply) {
+	  if(err) {
+	  	console.log('Error querying the database, returned:  ' + err);
+	  }
+    console.log(reply);
+    fs.readdir(__dirname + '/data/uploads/', function(err, files) {
+    	console.log(files);
+      	if(err) {
+        	console.log(err);
+          res.writeHead(500, {'content-type' : 'text/plain'});
+          res.write('There was an error processing your request. Please try again.');
+          res.end;
+        }
+
+        for(var i = 0;i<files.length;i++) {
+          if(files[i] === reply) {
+            console.log(files[i]);
+            res.writeHeader(200, {'Content-Type':'text.html'});
+            res.write('<a href=/download?url='+encodeURI(files[i])+'>Click to download your converted file</a>');
+            res.end();
+          }
+        }
+        
+        res.writeHead(404, {'Content-Type' : 'text/html'});
+        res.end('No files found.  Your download link has likely expired');   
+    }	);
+
+	  client.quit();
+	});
 }
 
 var download = function(res, req, query) {
-    console.log('download function called with query string '+query);
-    var mime = require('mime');
-    var filename = decodeURI(query);
-    var file = __dirname+'/data/uploads/'+filename;
-    var mimetype = mime.lookup(file);
-    
-    res.setHeader('Content-disposition', 'attachment; filename='+filename);
-    res.setHeader('Content-type', mimetype);
-                    
-    var filestream = fs.createReadStream(file, function(err) {
-      if(err) console.log(err);
-    });
-        filestream.on('data', function(chunk) {
-          res.write(chunk);
-        });
-        filestream.on('end', function() {
-          res.end();
-        });              
+	console.log('download function called with query string '+query);
+	var mime = require('mime');
+	var filename = decodeURI(query);
+	var file = __dirname+'/data/uploads/'+filename;
+	var mimetype = mime.lookup(file);
+
+	res.setHeader('Content-disposition', 'attachment; filename='+filename);
+	res.setHeader('Content-type', mimetype);
+                
+	var filestream = fs.createReadStream(file, function(err) {
+	  if(err) console.log(err);
+	});
+	
+  filestream.on('data', function(chunk) {
+    res.write(chunk);
+  });
+  filestream.on('end', function() {
+    res.end();
+  });              
 }
 
 var alphaRandom = function() {
-    console.log("generating download key");
-    var string = "";
-    var range = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    
-    for( var i=0; i < 8; i++ ) {
-      string += range.charAt(Math.floor(Math.random() * range.length));
-    }
-    return string;
+	console.log("generating download key");
+	var string = "";
+	var range = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+	for( var i=0; i < 8; i++ ) {
+	  string += range.charAt(Math.floor(Math.random() * range.length));
+	}
+	return string;
 }
 
 exports.upload = upload;
