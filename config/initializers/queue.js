8dfef841a5c8850fa0b075186e660ea1cedf0876
Adam Biagianti <adam.biagianti@gmail.com>
2014-01-05 18:24:54 -0800

    adds rough wrapper for kue

--- config/initializers/queue.js
+++ config/initializers/queue.js
@@ -1,18 +1,8 @@
-/* Initialize an instance of kue using user supplied redis configs, then
- * return a jobQueue to store and process jobs
-*/
-
-var kue = require("kue"),
-    redis = require("redis");
+var Queue = require(GLOBAL.dirname + "/lib/queue.js");
 
 module.exports = function(port, redisConfigs, app) {
-  kue.redis.createClient = function() {
-    return redis.createClient(redisConfigs.port, redisConfigs.host);
-  };
+  var queue = new Queue(port, redisConfigs, app, true);
 
-  kue.app.listen(port);
-
-  console.log("Queue created on port %d", port);
-  return kue.createQueue();
+  return queue.createQueue();
 };
 
